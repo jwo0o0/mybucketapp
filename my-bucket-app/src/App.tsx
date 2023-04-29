21c22a53cd5f63edecdbde26189801f49863b679
jwo0o0 <jwo0o0358@gmail.com>
2023-04-29 15:45:05 +0900

    version 2.0

--- my-bucket-app/src/App.tsx
+++ my-bucket-app/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React!!</h1>
+      <div>version 2.0</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
